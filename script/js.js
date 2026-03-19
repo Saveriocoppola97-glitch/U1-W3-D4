b8a65acabce988beb7ec722420c64ce5dff99777
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-03-19 17:36:48 +0100

    exercise fix bug by Stefano <3

--- script/js.js
+++ script/js.js
@@ -15,7 +15,8 @@ const numeriEstratti = []; //ho creato un arrey per i numeri lanciati
 const button = document.querySelector("button"); // ho selezionato il bottone tramite html
 
 const lancio = function () {
-  //creo una funzione chiamata lancio
+  // creo una funzione lancio
+
   return Math.floor(Math.random() * 99) + 1; //che ritorna un numero elevato casuale da 1 a 99
 };
 
@@ -24,6 +25,8 @@ button.addEventListener("click", function () {
   const randomnum = lancio(); // collego la funzione alla costante
   numeriEstratti.push(randomnum); // pusho il numero nella costante
   console.log(randomnum);
+  const ultimoEstratto = document.getElementById("ultimoEstratto");
+  ultimoEstratto.innerText = randomnum;
 
   const cella = document.getElementById("cella-" + randomnum);
   if (cella) {
